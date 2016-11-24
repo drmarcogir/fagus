d953ee2d4f96f1518ba7699ab00b629680ee0e0d
marcog77 <marco.girardello@gmail.com>
2016-11-24 13:51:33 -0100

    v1

---
+++ AnalysesMasterScript.R
@@ -0,0 +1,37 @@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Analyses species pools and local communities
+# Script created by Marco Girardello 19/02/2016 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# load required libraries
+library(dismo);library(WriteXLS);library(piecewiseSEM)
+library(lme4);library(pgirmess);library(vegan)
+library(stringi);library(nlme);library(MASS)
+library(stringr);library(boot)
+
+# source required functions
+source("/mnt/data1tb/Dropbox/Fagus/scripts/functions.R")
+
+# read in data
+dat<-read.csv("/mnt/data1tb/Dropbox/Fagus/dataJuly16/newdata.csv")
+# standardize variables
+source("/mnt/data1tb/Dropbox/Fagus/scripts/dataprepare.R")
+
+# fit sem models
+
+
+# fit BRT models
+
+
+# output for BRT models
+
+
+# output for SEM models 
+
+
+
+
+
+
+
+
