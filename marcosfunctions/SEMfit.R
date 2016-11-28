6780dcf3c4314e3b5c416b1f7008583b3c6400a7
marcog77 <marco.girardello@gmail.com>
2016-11-28 19:28:58 -0100

    v1v1

--- marcosfunctions/SEMfit.R
+++ marcosfunctions/SEMfit.R
@@ -4,18 +4,14 @@
 #13/07/2016 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-
-
 fitsem_wrapper<-function(.){
-  
-  # model for SR1 (no species pool)
+    # model for SR1 (no species pool)
   sr1nopool=list(
     m1<-glm(SR1~LGMS+DIST+TOPO+ATEMP+APREC+SPREC+PLOT+PH+L+T+M+N+R,data=dat.rpool1,family=poisson),
     m2<-glm(R~PH,data=dat.rpool1,family=gaussian),
     m3<-glm(T~ATEMP,data=dat.rpool1,family=gaussian),
     m4<-glm(M~APREC+SPREC,data=dat.rpool1,family=gaussian),
     m5<-glm(N~APREC+SPREC,data=dat.rpool1,family=gaussian))
-  
   # model for SR1 (with species pool)
   sr1pool=list(
     m1<-glm(Rpool1~LGMS+DIST+TOPO+ATEMP+APREC+SPREC,data=dat.rpool1,family=gaussian),
@@ -26,7 +22,6 @@ fitsem_wrapper<-function(.){
     m5<-glm(M~APREC+SPREC,data=dat.rpool1,family=gaussian),
     m6<-glm(N~APREC+SPREC,data=dat.rpool1,family=gaussian)
   )
-  
   # model for SR2 (no species pool)
   sr2nopool=list(
     m1<-glm(SR2~LGMS+DIST+TOPO+ATEMP+APREC+SPREC+PLOT+PH+L+T+M+N+R,data=dat.rpool2,family=poisson),
@@ -35,7 +30,6 @@ fitsem_wrapper<-function(.){
     m4<-glm(M~APREC+SPREC,data=dat.rpool2,family=gaussian),
     m5<-glm(N~APREC+SPREC,data=dat.rpool2,family=gaussian)
   )
-  
   # model for SR2 (with species pool)
   sr2pool=list(
     m1<-glm(Rpool2~LGMS+DIST+TOPO+ATEMP+APREC+SPREC,data=dat.rpool2,family=gaussian),
@@ -45,9 +39,7 @@ fitsem_wrapper<-function(.){
     m5<-glm(M~APREC+SPREC,data=dat.rpool2,family=gaussian),
     m6<-glm(N~APREC+SPREC,data=dat.rpool2,family=gaussian)
   )
-  
   # Geometric species pools
-  
   # model for SR1 (no species pool)
   g1nopool=list(
     m1<-glm(SR1~LGMS+DIST+TOPO+ATEMP+APREC+SPREC+PLOT+PH+L+T+M+N+R,data=dat.gpool1,family=poisson),
@@ -56,7 +48,6 @@ fitsem_wrapper<-function(.){
     m4<-glm(M~APREC+SPREC,data=dat.gpool1,family=gaussian),
     m5<-glm(N~APREC+SPREC,data=dat.gpool1,family=gaussian)
   )
-  
   # model for SR1 (with species pool)
   g1pool=list(
     m1<-glm(Gpool1~LGMS+DIST+TOPO+ATEMP+APREC+SPREC,data=dat.gpool1,family=gaussian),
@@ -66,7 +57,6 @@ fitsem_wrapper<-function(.){
     m5<-glm(M~APREC+SPREC,data=dat.gpool1,family=gaussian),
     m6<-glm(N~APREC+SPREC,data=dat.gpool1,family=gaussian)
   )
-  
   # model for SR2 (no species pool)
   g2nopool=list(
     m1<-glm(SR2~LGMS+DIST+TOPO+ATEMP+APREC+SPREC+PLOT+PH+L+T+M+N+R,data=dat.gpool2,family=poisson),
@@ -75,7 +65,6 @@ fitsem_wrapper<-function(.){
     m4<-glm(M~APREC+SPREC,data=dat.gpool2,family=gaussian),
     m5<-glm(N~APREC+SPREC,data=dat.gpool2,family=gaussian)
   )
-  
   # model for SR2 (with species pool)
   g2pool=list(
     m1<-glm(Gpool2~LGMS+DIST+TOPO+ATEMP+APREC+SPREC,data=dat.gpool2,family=gaussian),
@@ -85,9 +74,7 @@ fitsem_wrapper<-function(.){
     m5<-glm(M~APREC+SPREC,data=dat.gpool2,family=gaussian),
     m6<-glm(N~APREC+SPREC,data=dat.gpool2,family=gaussian)
   )
-  
   allmodels<-list(sr1nopool,sr1pool,sr2nopool,sr2pool,g1nopool,g1pool,g2nopool,g2pool)
   names(allmodels)<-c("sr1nopool","sr1pool","sr2nopool","sr2pool","g1nopool","g1pool","g2nopool","g2pool")
-  
   return(allmodels)
 }
