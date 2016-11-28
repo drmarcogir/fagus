a31e67ddfd4bb6a328ca255868a54381b302aa69
marcog77 <marco.girardello@gmail.com>
2016-11-28 19:26:59 -0100

    v1

--- marcosfunctions/supportfunctions.R
+++ marcosfunctions/supportfunctions.R
@@ -1,70 +1,8 @@
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Supporting functions for Fagus
+# Support functions for Fagus
 # data analysis project. Created
 # by Marco Girardello 19/09/2016
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-
-####################################
-# get dataset used for fitting 
-# spatially explicit  SEMs
-####################################
-
-dataget <- function(inputdf, sac) {
-        mod.no <- unique(inputdf$listno)    
-    # list of lists
-    allsem <- vector("list", length(mod.no))
-    names(allsem) <- unique(inputdf$sem.name)    
-# individual sem models
-for (i in 1:length(mod.no)) { 
-  moddf <- subset(inputdf, listno == mod.no[i])
-  # results for individual sem model
-  if (sac == "FALSE") {
-    pathd <- paste(moddf[1, ]$nosacpath, "/datmod", moddf[1, ]$modID,".csv", sep = "")
-    dat <- read.csv(pathd)
-    allsem[[i]] <-dat  
-} else {
-    pathd <- paste(moddf[1, ]$sacpath, "/datmod", moddf[1, ]$modID,".csv", sep = "")
-    dat <- read.csv(pathd)
-    allsem[[i]] <-dat
-    
-  }
-}  
-    return(allsem)    
-}
-
-####################################
-# get SEM models 
-####################################
-
-semget <- function(inputdf, sac) {    
-    mod.no <- unique(inputdf$listno)   
-    # list of lists
-    allsem <- vector("list", length(mod.no))
-    names(allsem) <- unique(inputdf$sem.name)
-    # individual sem models
-    for (i in 1:length(mod.no)) {
-         moddf <- subset(inputdf, listno == mod.no[i]) 
-        # results for individual sem model
-        singlesem <- vector("list", dim(moddf)[1])
-        if (sac == "FALSE") {
-            for (x in 1:dim(moddf)[1]) {
-                pathm <- paste(moddf[x, ]$nosacpath, "/mod", moddf[x, ]$modID, sep = "")
-                mod <- get(load(pathm))
-                singlesem[[x]] <- mod   
-            }            
-        } else { 
-            for (x in 1:dim(moddf)[1]) { 
-                pathm <- paste(moddf[x, ]$sacpath, "/mod", moddf[x, ]$modID, sep = "")
-                mod <- get(load(pathm))
-                singlesem[[x]] <- mod 
-            }
-        }
-        allsem[[i]] <- singlesem 
-    }
-    return(allsem)
-}
-
 
 
 #######################################
